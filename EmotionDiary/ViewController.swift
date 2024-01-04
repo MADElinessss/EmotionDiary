8534fb9405a36bb29cd898dd6356a88188946be3
MADElinessss <jyseen@naver.com>
2024-01-04 20:34:40 +0900

    userdefaults에 데이터 저장

--- EmotionDiary/ViewController.swift
+++ EmotionDiary/ViewController.swift
@@ -18,26 +18,44 @@ class ViewController: UIViewController {
     @IBOutlet var label7: UILabel!
     @IBOutlet var label8: UILabel!
     @IBOutlet var label9: UILabel!
-    
-    var tapTimes: [UILabel : Int] = [:]
+
     var originalTexts: [UILabel : String] = [:]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let labels = [label1, label2, label3, label4, label5, label6, label7, label8, label9]
+        // originalTexts 초기값 설정
         for label in labels {
-            tapTimes[label!] = 0
             originalTexts[label!] = label?.text
         }
+        updateTapTimes()
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
         let labels = [label1, label2, label3, label4, label5, label6, label7, label8, label9]
+        // tag: 1~9
+        let index = sender.tag - 1
+        if let label = labels[index] {
+            print("index = \(index)")
+            let tapTimes = UserDefaults.standard.integer(forKey: "tapTimes\(index)") + 1
+            UserDefaults.standard.set(tapTimes, forKey: "tapTimes\(index)")
+            let updatedTap = UserDefaults.standard.integer(forKey: "tapTimes\(index)")
+            if let originalLabel = originalTexts[label] {
+                label.text = "\(originalLabel) \(updatedTap)"
+            }
+        }
+    }
+    
+    func updateTapTimes() {
+        print("updateTapTimes")
+        let labels = [label1, label2, label3, label4, label5, label6, label7, label8, label9]
         
-        if let label = labels[sender.tag - 1] {
-            tapTimes[label]! += 1
-            label.text! = "\(originalTexts[label] ?? "") \(tapTimes[label]!)"
+        for index in 0...8 {
+            let key = "tapTimes\(index)"
+            let lastTapTimes = UserDefaults.standard.integer(forKey: key)
+            print(lastTapTimes)
+            labels[index]?.text = "좋아해 \(lastTapTimes)"
         }
     }
     
